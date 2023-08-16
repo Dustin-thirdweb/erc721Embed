bff0f6af941db141fb7b198b41c10c58c1aba77d
Dustin <135719141+Dustin-thirdweb@users.noreply.github.com>
2023-08-16 12:51:09 -0500

    Add files via upload

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -13,7 +13,7 @@ export const chainConst = "";
 export const primaryColorConst = "";
 
 // Choose between "light" and "dark" mode
-export const themeConst = "";
+export const themeConst = "dark";
 
 // Gasless relayer configuration options
 export const relayerUrlConst = ""; // OpenZeppelin relayer URL
@@ -22,5 +22,5 @@ export const biconomyApiIdConst = ""; // Biconomy API ID
 export const clientIdConst = ""; // Client ID
 
 // Wallet connect configuration options
-export const paperClientIdConst = ""; // Paper wallet client ID
-export const magicLinkApiKeyConst = ""; // Magic link API key
+export const paperClientIdConst = "f4d2fc79-523f-4b45-ba1c-32ffa8a53872"; // Paper wallet client ID
+export const magicLinkApiKeyConst = "pk_live_5BD2E64C36DC78AA"; // Magic link API key
